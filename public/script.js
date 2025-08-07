030cf88a2573698b5458e0aea4397e4bcc8f8658
rayangithub1 <rayanasif1080@gmail.com>
2025-08-07 17:09:06 +0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -241,10 +241,10 @@ function appendMessage(message, type, name = '') {
     nameSpan.style.marginRight = '5px';
 
     if (type === 'user') {
-        nameSpan.textContent = `${username}`;
+        nameSpan.textContent = `${username}:`;
         nameSpan.style.color = 'blue';
     } else if (type === 'partner') {
-        nameSpan.textContent = `${name}`;
+        nameSpan.textContent = `${name}:`;
         nameSpan.style.color = 'red';
     } else if (type === 'system') {
         const existingSystemMessages = messagesDiv.querySelectorAll('.system-message');
